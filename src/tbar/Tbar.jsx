fd98502b8fa0d2d1e5b1ff68627931bd7a9f7cde
luharine <72065741+luharine@users.noreply.github.com>
2021-10-12 00:55:08 +0530

    updated Tbar.jsx

--- src/tbar/Tbar.jsx
+++ src/tbar/Tbar.jsx
@@ -5,11 +5,14 @@ export default function Tbar() {
         <div class = 'Tbar'>
                 
                
-                   <div class="left"><i class="fab fa-discord"></i>
-                                     <i class="fab fa-twitter"></i>
-                                     <i class="fab fa-pinterest-p"></i> 
-                                     <i class="fab fa-reddit-alien"></i>
-                                     <i class="fab fa-instagram"></i>
+                   <div class="left">
+                       <div class="top-icons">
+                                     <i class="top-icon fab fa-discord"></i>
+                                     <i class="top-icon fab fa-twitter"></i>
+                                     <i class="top-icon fab fa-pinterest-p"></i> 
+                                     <i class="top-icon fab fa-reddit-alien"></i>
+                                     <i class="top-icon fab fa-instagram"></i>
+                        </div>
                    </div>
                    <div class="centre">
                                   <ul class="top-center-list" horizontal>
@@ -23,8 +26,9 @@ export default function Tbar() {
 
                    </div>
                    <div class = "right">
-
+                   
                        <img className="top-img" src = "/images/girl.jpg" alt="a beautiful girl"/>
+                        <i class="searchimg fas fa-search"></i>
                        
                     </div>
                 
