646ba55debac38cb5c70e358a06f6a6ab632f159
luharine <72065741+luharine@users.noreply.github.com>
2021-10-11 23:31:05 +0530

    added topbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import Header from "../../header/Header"
+import Tbar from "../../tbar/Tbar"
 import "./home.css"
+
+
+
 
 export default function home() {
     return (
         <div className="home">
+             <Tbar />
             <Header />
+            
             Homepage
         </div>
     )
---
+++ src/tbar/Tbar.jsx
@@ -0,0 +1,33 @@
+import "./tbar.css"
+
+export default function Tbar() {
+    return (
+        <div class = 'Tbar'>
+                
+               
+                   <div class="left"><i class="fab fa-discord"></i>
+                                     <i class="fab fa-twitter"></i>
+                                     <i class="fab fa-pinterest-p"></i> 
+                                     <i class="fab fa-reddit-alien"></i>
+                                     <i class="fab fa-instagram"></i>
+                   </div>
+                   <div class="centre">
+                                  <ul class="top-center-list" horizontal>
+                                       <li class = "top-center-list-item">HOME</li>
+                                       <li class = "top-center-list-item">ABOUT</li>
+                                       <li class = "top-center-list-item">CONTACT</li>
+                                       <li class = "top-center-list-item">INK</li>
+                                       <li class = "top-center-list-item">LOGOUT</li>  
+                                  </ul>  
+
+
+                   </div>
+                   <div class = "right">
+
+                       <img className="top-img" src = ".//.//public\images\girl.jpg"/>
+                       
+                    </div>
+                
+        </div>
+    )
+}
