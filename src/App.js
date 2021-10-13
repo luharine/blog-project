9a827d5f8824dbf3bf9bc8835ee2a43bd1159401
GnaniPasupula <gnanipasupula@gmail.com>
2021-10-13 12:57:46 +0530

    Fixed Posts and Header Side to side

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Tbar from "./tbar/Tbar";
 
 function App() {
   return (
