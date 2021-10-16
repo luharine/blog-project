dbe1b198bd712c198b7ea86b3002bdea2ceaaf20
GnaniPasupula <gnanipasupula@gmail.com>
2021-10-16 15:23:58 +0530

    Created Express Server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+
+const app= express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>{
+    console.log(err)
+});
+
+
+app.listen("5000",()=>{
+    console.log("Server isrunning");
+});
