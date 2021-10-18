e75a0dbdc115990d49432d53a3a2f2c4ff828cbe
GnaniPasupula <gnanipasupula@gmail.com>
2021-10-18 15:48:57 +0530

    Finished User Authentication

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-
+const authRoute = require("./routes/auth");
 const app= express();
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MongoDB")).catch(err=>{
     console.log(err)
 });
 
+app.use("/api/auth",authRoute);
 
 app.listen("5000",()=>{
     console.log("Server is running");
