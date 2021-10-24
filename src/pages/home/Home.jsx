0caf0bc380267e0b07e8d59101a4261d0c63fdb7
luharine <72065741+luharine@users.noreply.github.com>
2021-10-24 16:57:31 +0530

    updated:..

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,21 +4,28 @@ import Header from "../../header/Header"
 import Posts from "../../posts/Posts";
 import Sbar from "../../sbar/Sbar";
 import "./home.css"
+import {useState} from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Home() {
+  const [posts,setposts]=useState([]);
 
-  const [posts,setPosts]=useState([]);
-
-  useEffect(()=>{
-    const fetchPosts=async()=>{
-      const res=await axios.get("/posts");
-      setPosts(res.data);
-      console.log(res);
+  useEffect(() => {
+    const fetchPosts = async () =>
+    {
+     const res = await axios.get("/posts")
+     console.log(res)
+    setposts(res.data)
     }
-    fetchPosts();
-  },[])
-
-    return (    
+     fetchPosts();
+    
+  }
+   ,[]
+  )
+   
+    return (
+        
     <>
       <Header />    
         
